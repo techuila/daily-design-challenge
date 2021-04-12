5eb0962d291db7f8fe18352b13c2672ef129ca08
techuila <axeguns@gmail.com>
2021-04-13 00:51:09 +0800

    Day 1

---
+++ Day 1/README.md
@@ -0,0 +1,8 @@
+# Day 1
+
+## Cryptocurrency App
+
+Designed by [Ofspace Digital Agency](https://dribbble.com/ofspacedesign)<br>
+[Link to dribble](https://dribbble.com/shots/15440405-Cryptocurrency-App-I-Ofspace/attachments/7210105?mode=media)
+
+![alt text](https://cdn.dribbble.com/users/2125046/screenshots/15440405/media/1fbceabfef899736993a01d32e590fc5.png)
---
+++ Day 1/assets/js/index.js
@@ -0,0 +1,45 @@
+$(document).ready(() => {
+	function main() {
+		// Display header
+		if (['', '#home'].includes(window.location.hash)) {
+			$('header').load('/components/HomeHeader.html');
+		}
+
+		// Listen for window resize
+		resizeFn();
+		$(window).resize(resizeFn);
+		$('main').on('scroll', scrollFn);
+	}
+
+	main();
+});
+
+function resizeFn() {
+	const main_el = $('main');
+	const nav_el = $('#bottom-navigation');
+	const target_class = 'scrollable-main';
+	const hasScrollBar =
+		main_el.get(0).scrollHeight >
+		$('#app').get(0).scrollHeight -
+			($('header').get(0).scrollHeight + $('footer').get(0).scrollHeight);
+
+	if (hasScrollBar && !nav_el.hasClass(target_class)) {
+		nav_el.toggleClass(target_class);
+	} else if (!hasScrollBar && nav_el.hasClass(target_class)) {
+		nav_el.toggleClass(target_class);
+	}
+}
+
+function scrollFn() {
+	const nav_el = $('#bottom-navigation');
+	const target_class = 'scrollable-main';
+
+	if (
+		$(this).scrollTop() ===
+		$(this).get(0).scrollHeight - $(this).get(0).offsetHeight
+	) {
+		nav_el.toggleClass(target_class);
+	} else if (!nav_el.hasClass(target_class)) {
+		nav_el.toggleClass(target_class);
+	}
+}
